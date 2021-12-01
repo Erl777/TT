56b57a72ccf69e69570f1be9e9a2b33b3427805b
Erl777 <erl.pank777@gmail.com>
2021-12-01 16:55:14 +0300

    header + banner

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,74 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header class="header">
+      <img src="/img/Logo.svg" class="logo__img" alt="">
+      <nav class="nav">
+        <ul class="menu">
+          <li class="menu__item">
+            <a href="#" class="menu__link">About me</a>
+          </li>
+          <li class="menu__item">
+            <a href="#" class="menu__link">Relationships</a>
+          </li>
+          <li class="menu__item">
+            <a href="#" class="menu__link">Requirements</a>
+          </li>
+          <li class="menu__item">
+            <a href="#" class="menu__link">Users</a>
+          </li>
+          <li class="menu__item">
+            <a href="#" class="menu__link">Sign Up</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <Home />
+    <footer>
+
+    </footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './views/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import "src/assets/css/styles";
+  .header {
+    height: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: #fff;
+    padding: 0 60px;
+    box-shadow: -1px -1px 5px rgba(0, 0, 0, 0.15), 0px 4px 15px rgba(0, 0, 0, 0.15);
+  }
+  .nav {
+    width: 53.65%;
+  }
+  .menu {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+    &__item:not(:last-child) {
+      margin-right: 30px;
+    }
+    &__link {
+      text-decoration: none;
+      color: #000;
+      white-space: nowrap;
+      // 489
+    }
+  }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,58 @@
+<template>
+  <main>
+    <section class="container introduction">
+      <h1 class="main-title"> Test assignment for front-end developers </h1>
+      <p class="introduction__description">
+        Front-end developers make sure the user sees and interacts with all
+        the necessary elements to ensure conversion. Therefore, responsive design,
+        programming languages and specific frameworks are the must-have skillsets
+        to look for when assessing your front-end developers.
+      </p>
+      <button class="yellow-btn">Sign up</button>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  header, footer, main {
+    max-width: 1400px;
+    margin: 0 auto;
+  }
+  .introduction {
+    height: 650px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding-left: 60px;
+    background-image: url("/img/Banner_photo.jpg");
+    background-size: cover;
+    background-position: center;
+    &__description {
+      max-width: 527px;
+      margin: 0 0 25px 0;
+    }
+  }
+  .main-title {
+    width: 100%;
+    max-width: 670px;
+    margin: 0 0 20px 0;
+  }
+  .yellow-btn {
+    width: 148px;
+    height: 50px;
+    border-radius: 80px;
+    background-color: #F4E041;
+    font-family: 'Nunito', sans-serif;
+    font-size: 16px;
+    font-weight: 400;
+  }
+</style>
