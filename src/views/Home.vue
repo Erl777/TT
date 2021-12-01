a7eb216e398dd1f45daa41ebe2d88329df5db8af
Erl777 <erl.pank777@gmail.com>
2021-12-01 17:41:31 +0300

    acquaintance

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <main>
     <section class="container introduction">
-      <h1 class="main-title"> Test assignment for front-end developers </h1>
+      <h1 class="introduction__title"> Test assignment for front-end developers </h1>
       <p class="introduction__description">
         Front-end developers make sure the user sees and interacts with all
         the necessary elements to ensure conversion. Therefore, responsive design,
@@ -9,6 +9,24 @@
         to look for when assessing your front-end developers.
       </p>
       <button class="yellow-btn">Sign up</button>
+    </section>
+    <section class="container acquaintance">
+      <img src="/img/Image-387x340.svg" alt="">
+      <div class="acquaintance__content">
+        <h1 class="acquaintance__title">
+          Let's get acquainted
+        </h1>
+        <h2 class="acquaintance__subtitle">
+          I'm a good front-end developer
+        </h2>
+        <p class="acquaintance__description">
+          What defines a good front-end developer is one that has skilled knowledge of HTML,
+          CSS, JS with a vast understanding of User design thinking as they'll be building
+          web interfaces with accessibility in mind. They should also be excited to learn,
+          as the world of Front-End Development keeps evolving.
+        </p>
+        <button class="yellow-btn">Sign up</button>
+      </div>
     </section>
   </main>
 </template>
@@ -27,6 +45,9 @@ export default {
     max-width: 1400px;
     margin: 0 auto;
   }
+  .container {
+    padding: 0 60px;
+  }
   .introduction {
     height: 650px;
     display: flex;
@@ -36,16 +57,18 @@ export default {
     background-image: url("/img/Banner_photo.jpg");
     background-size: cover;
     background-position: center;
+    margin-bottom: 175px;
+    &__title {
+      width: 100%;
+      max-width: 670px;
+      margin: 0 0 20px 0;
+    }
     &__description {
       max-width: 527px;
       margin: 0 0 25px 0;
     }
   }
-  .main-title {
-    width: 100%;
-    max-width: 670px;
-    margin: 0 0 20px 0;
-  }
+
   .yellow-btn {
     width: 148px;
     height: 50px;
@@ -54,5 +77,27 @@ export default {
     font-family: 'Nunito', sans-serif;
     font-size: 16px;
     font-weight: 400;
+  }
+  .acquaintance {
+    display: flex;
+    justify-content: space-between;
+    text-align: right;
+    margin-bottom: 175px;
+    &__content {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-end;
+      padding-top: 16px;
+    }
+    &__title {
+      margin: 0 0 10px 0;
+    }
+    &__subtitle {
+      margin: 0 0 19px 0;
+    }
+    &__description {
+      max-width: 477px;
+      margin: 0 0 32px 0;
+    }
   }
 </style>
